1003bdca807155d30202ecadde3e3557efe4994a
DominusWolz <n.diaz115@alumnos.santotomas.cl>
2026-04-06 00:11:11 -0400

    Add files via upload
    Cambios en los botones y añadi el formulario

--- scripbuton.js
+++ scripbuton.js
@@ -1,14 +1,13 @@
-document.getElementById('dynamicButton').addEventListener('click', function() {
+document.getElementById('dynamicButton').addEventListener('click', function () {
     var Nombre = document.getElementById('nombre').value;
     var Apellido = document.getElementById('apellido').value;
     var NumTelefonico = document.getElementById('numtelefonico').value;
     var CorreoElectronico = document.getElementById('correoelectronico').value;
     var OpSug = document.getElementById('opsug').value;
 
-    if(Nombre === "", Apellido === "", NumTelefonico === "", CorreoElectronico === "", OpSug === ""){
+    if (Nombre === "" || Apellido === "" || NumTelefonico === "" || CorreoElectronico === "" || OpSug === "") {
         alert(" Ingrese Informacion en los Campos Vacios Porfavor");
-        } else {
-            window.location.href = "Formulario.html"
-        }
+    } else {
+        window.location.href = "Formulario.html"
     }
-);
+});
---
+++ scripvolver.js
@@ -0,0 +1,3 @@
+document.getElementById('volver').addEventListener('click', function() {
+    window.location.href = "Portada.html"; // asegúrate que el archivo exista con ese nombre exacto
+});
